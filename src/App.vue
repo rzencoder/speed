8b761977f64284a112fd0c7acac2a780474c31bb
adam <hamer88@hotmail.co.uk>
2017-10-30 13:14:54 +0000

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <Main/>
+  </div>
+</template>
+
+<script>
+
+import Main from './components/main'
+
+export default {
+  name: 'app',
+  components: {
+    Main
+  }
+}
+
+</script>
+
+<style lang="scss">
+
+body {
+  font-family: 'Courier', Helvetica, Arial, monospace;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #fff;
+  background: #222;
+  
+}
+
+@font-face {
+    font-family: speed;
+    src: url(./assets/speed.ttf);
+}
+
+h1 {
+  font-family: speed, sans-serif;
+  color: red;
+  font-size: 40px;
+  margin: 0;
+  padding: 0px;
+}
+
+h2 {
+  font-family: speed, sans-serif;
+  color: red;
+  font-size: 40px;
+  margin: 0;
+  padding: 0px;
+}
+
+</style>
---
+++ src/assets/lyrics.js
@@ -0,0 +1,18 @@
+const lyrics = {   
+    "easy": [
+        "We're on a road to nowhere, come on inside. Taking that ride to nowhere, we'll take that ride. I'm feeling okay this morning, and you know. We're on the road to paradise. Here we go, here we go.",
+        "Just a small town girl, livin' in a lonely world. She took the midnight train, goin' anywhere. Just a city boy, born and raised in South Detroit. He took the midnight train, goin' anywhere.",
+        "Revvin' up your engine. Listen to her howlin' roar. Metal under tension. Beggin' you to touch and go. Highway to the danger zone. Ride into the danger zone"
+    ],
+    "medium": [
+        "In the day we sweat it out on the streets of a runaway American dream. At night we ride through the mansions of glory in suicide machines. Sprung from cages out on highway nine, Chrome wheeled, fuel injected, and steppin' out over the line H-Oh. Baby this town rips the bones from your back. It's a death trap, it's a suicide rap. We gotta get out while we're young. 'Cause tramps like us, baby we were born to run",
+        "Rising up, back on the street.Did my time, took my chances.Went the distance, now I'm back on my feet. Just a man and his will to survive. It's the eye of the tiger.It's the thrill of the fight. Rising up to the challenge of our rival. And the last known survivor. Stalks his prey in the night. And he's watching us all with the eye of the tiger."
+
+    ],
+    "hard": [
+        "I'm a shooting star leaping through the skies. Like a tiger defying the laws of gravity. I'm a racing car passing by like Lady Godiva. I'm gonna go go go, there's no stopping me. I'm burning through the sky yeah! Two hundred degrees, that's why they call me Mister Fahrenheit. I'm trav'ling at the speed of light. I wanna make a supersonic man out of you. Don't stop me now. I'm having such a good time. I'm having a ball. Don't stop me now. If you wanna have a good time. Just give me a call. Don't stop me now. Don't stop me now. I don't want to stop at all... yeah!",
+        "I'm a puppet on a string. Tracy island, time - traveling diamond. Could a shaped heartaches. Come to find ya fall in some velvet morning. Years too late. She's a silver lining lone ranger riding. Through an open space. In my mind when she's not right there beside me. I go crazy 'cause here isn't where I wanna be. And satisfaction feels like a distant memory. And I can't help myself. All I wanna hear her say is are you mine? Are you mine? Are you mine? Are you mine?"
+    ]
+};
+
+export default lyrics;
